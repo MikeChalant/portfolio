7d52cec0c9316c8198ef84e93e84011f708accd2
mikechalant <mikechalant@gmail.com>
2022-11-03 03:00:38 -1200

    update

---
+++ sort.js
@@ -0,0 +1,23 @@
+[
+    {
+        name:'Tyronne Williams',
+        title:'Senior Manager',
+        desc:'Hardworker Goodlooks Sexy Too',
+        rating:3,
+        img_src:'l2.jpg'
+    },
+    {
+        name: 'Jame Williams',
+        title: 'Senior Manager',
+        desc: 'Hardworker Goodlooks Sexy Too',
+        rating: 4,
+        img_src:'check-view.jpg'
+    },
+    {
+        name: 'Faithful Williams',
+        title: 'Senior Manager',
+        desc: 'Hardworker Goodlooks Sexy Too',
+        rating: 5,
+        img_src:'4.png'
+    }
+]
